0e5eba759e69835d91b5e3e0795c191434c15b8f
XieYu <xieyumc@gmail.com>
2024-07-08 02:46:51 +0800

    feature：astro现在可以从Django中获取web的配置

--- astro/src/consts.js
+++ astro/src/consts.js
@@ -18,3 +18,21 @@ export const Footer2_Website1_ulr = "/";
 export const Footer3_Title = "作者";
 export const Footer3_Website1_title = "宇宇";
 export const Footer3_Website1_ulr = "https://github.com/xieyumc";
+
+export async function getConfig() {
+
+    try {
+        const response = await fetch(process.env.WEBCONFIG_API_URL || 'http://127.0.0.1:8000/api/web_config');
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Failed to fetch config, using default.', error);
+        return defaultConfig;
+    }
+}
+
+// 调用 getConfig 并导出配置
+export const config = await getConfig();
