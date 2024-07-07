4c15c035096f853f997c6606c0ab9b67e2c9821d
XieYu <xieyumc@gmail.com>
2024-07-08 01:13:04 +0800

    remove:移除过时文件

--- astro/src/consts.js
+++ astro/src/consts.js
@@ -1,11 +1,12 @@
-export const SITE_TITLE = `天际水域`;
-export const SITE_DESCRIPTION = 'Site Description';
+export const SITE_TITLE = `鱼鱼幼稚园`;
+export const SITE_DESCRIPTION = '记录鱼鱼的随笔';
 export const SITE_EMAIL = 'xieyumc@gmail.com'
-export const SITE_NAME = '天际水域';
-export const SITE_URL = "https://web-homework2024.vercel.app";
+export const SITE_NAME = '鱼鱼幼稚园';
+export const SITE_URL = "https://github.com/xieyumc/AppleBlog";
 export const SITE_LANG = "zh-CN";
 
+
+
 // Author, used only when author is not specified on the page, 
-// has lower priority than the author specified in the md file.
-// 作者信息，仅用在文章没有指定作者的时候，优先显示在 md 文件中指定的作者。
+// has lower priority than the author specified in the md file.// 作者信息，仅用在文章没有指定作者的时候，优先显示在 md 文件中指定的作者。
 export const SITE_AUTHOR = "谢宇";
