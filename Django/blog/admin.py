33bc0ed9c27c710625347e207c212d8369066023
XieYu <xieyumc@gmail.com>
2024-07-10 00:11:22 +0800

    fix：调整文章管理布局

--- Django/blog/admin.py
+++ Django/blog/admin.py
@@ -7,7 +7,7 @@ from .models import Post,Image
 # 注册Post模型
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'author', 'pub_date', 'featured', 'theme')
+    list_display = ( 'title','id', 'author', 'pub_date', 'featured', 'theme')
     search_fields = ('title', 'author')
     list_filter = ('featured', 'theme', 'pub_date')
     date_hierarchy = 'pub_date'
