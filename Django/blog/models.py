504a999a6a025556c9119943c09695b74af6644f
XieYu <xieyumc@gmail.com>
2024-07-09 23:54:04 +0800

    fix：调整文章数据布局

--- Django/blog/models.py
+++ Django/blog/models.py
@@ -7,13 +7,11 @@ class Post(models.Model):
     )
 
     title = models.CharField(max_length=200)
+    description = models.TextField()
     content = models.TextField()  # 存储Markdown内容
     pub_date = models.DateTimeField('date published')
-    description = models.TextField()
     author = models.CharField(max_length=100)
     cover_url = models.URLField(blank=True, null=True)
-    # cover_square = models.URLField(blank=True, null=True)  # 正方形封面图像URL
-    # cover_alt = models.CharField(max_length=255, blank=True, null=True)  # 封面图像描述
     tags = models.CharField(max_length=200, blank=True, null=True)
     theme = models.CharField(max_length=10, choices=THEME_CHOICES, default='light')  # 主题，限定为 'light' 或 'dark'
     featured = models.BooleanField(default=True)
